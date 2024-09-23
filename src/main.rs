a0638b5babdfcf715453a401e2da2ceec0b46dd7
af8a2a <1125209282@qq.com>
2024-09-23 22:47:15 +0800

    finish create instance

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use ash::Entry;
+use std::ffi::{self, c_char};
+
+use ash::ext::debug_utils;
+use ash::{vk, Entry};
 use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::raw_window_handle::HasDisplayHandle;
 use winit::window::{Window, WindowId};
 
 pub struct VulkanApp {
@@ -11,17 +15,59 @@ pub struct VulkanApp {
 }
 
 impl VulkanApp {
-    pub fn new() -> Self {
+    pub fn new(window: &Window) -> Self {
         let entry = Entry::linked();
         unsafe {
-            let instance_create_info = ash::vk::InstanceCreateInfo::default();
+
+            let app_name = ffi::CStr::from_bytes_with_nul_unchecked(b"VulkanTriangle\0");
+            let layer_names = [ffi::CStr::from_bytes_with_nul_unchecked(
+                b"VK_LAYER_KHRONOS_validation\0",
+            )];
+            let layers_names_raw: Vec<*const c_char> = layer_names
+                .iter()
+                .map(|raw_name| raw_name.as_ptr())
+                .collect();
+
+            let mut extension_names = ash_window::enumerate_required_extensions(
+                window
+                    .display_handle()
+                    .expect("Failed to get display handle")
+                    .as_raw(),
+            )
+            .expect("Failed to enumerate extensions")
+            .to_vec();
+            extension_names.push(debug_utils::NAME.as_ptr());
+
+            let appinfo = vk::ApplicationInfo::default()
+                .application_name(app_name)
+                .application_version(0)
+                .engine_name(app_name)
+                .engine_version(0)
+                .api_version(vk::make_api_version(0, 1, 0, 0));
+
+            let instance_create_info = vk::InstanceCreateInfo::default()
+                .application_info(&appinfo)
+                .enabled_layer_names(&layers_names_raw)
+                .enabled_extension_names(&extension_names)
+                .flags(vk::InstanceCreateFlags::default());
+
             let instance = entry
                 .create_instance(&instance_create_info, None)
-                .expect("Failed to create instance");
+                .expect("Instance creation error");
+
             Self { entry, instance }
         }
     }
 }
+
+impl Drop for VulkanApp {
+    fn drop(&mut self) {
+        unsafe {
+            self.instance.destroy_instance(None);
+        }
+    }
+}
+
 
 #[derive(Default)]
 struct App {
@@ -36,7 +82,7 @@ impl ApplicationHandler for App {
                 .create_window(Window::default_attributes())
                 .unwrap(),
         );
-        self.vk = Some(VulkanApp::new());
+        self.vk = Some(VulkanApp::new(self.window.as_ref().unwrap()));
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {

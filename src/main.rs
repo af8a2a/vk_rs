1e1e5f5687de724a3997afe19e4b915e8b708cbd
af8a2a <1125209282@qq.com>
2024-09-23 23:56:54 +0800

    renderpass create

--- src/main.rs
+++ src/main.rs
@@ -475,6 +475,63 @@ impl VulkanApp {
     }
 }
 
+impl VulkanApp {
+    pub fn render_loop(&mut self) {
+        unsafe {
+            let renderpass_attachments = [
+                vk::AttachmentDescription {
+                    format: self.surface_format.format,
+                    samples: vk::SampleCountFlags::TYPE_1,
+                    load_op: vk::AttachmentLoadOp::CLEAR,
+                    store_op: vk::AttachmentStoreOp::STORE,
+                    final_layout: vk::ImageLayout::PRESENT_SRC_KHR,
+                    ..Default::default()
+                },
+                vk::AttachmentDescription {
+                    format: vk::Format::D16_UNORM,
+                    samples: vk::SampleCountFlags::TYPE_1,
+                    load_op: vk::AttachmentLoadOp::CLEAR,
+                    initial_layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+                    final_layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+                    ..Default::default()
+                },
+            ];
+            let color_attachment_refs = [vk::AttachmentReference {
+                attachment: 0,
+                layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+            }];
+            let depth_attachment_ref = vk::AttachmentReference {
+                attachment: 1,
+                layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+            };
+            let dependencies = [vk::SubpassDependency {
+                src_subpass: vk::SUBPASS_EXTERNAL,
+                src_stage_mask: vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                dst_access_mask: vk::AccessFlags::COLOR_ATTACHMENT_READ
+                    | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+                dst_stage_mask: vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+                ..Default::default()
+            }];
+
+            let subpass = vk::SubpassDescription::default()
+                .color_attachments(&color_attachment_refs)
+                .depth_stencil_attachment(&depth_attachment_ref)
+                .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS);
+
+            let renderpass_create_info = vk::RenderPassCreateInfo::default()
+                .attachments(&renderpass_attachments)
+                .subpasses(std::slice::from_ref(&subpass))
+                .dependencies(&dependencies);
+
+            let renderpass = self
+                .device
+                .create_render_pass(&renderpass_create_info, None)
+                .unwrap();
+            
+        }
+    }
+}
+
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
